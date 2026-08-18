0133edc3f61220f60f0dd62eb058545c32ea15c0
emirhancelenk2011 <emirhan.celenk2011@gmail.com>
2026-08-18 19:12:55 +0300

    Fix Parking imports

--- src/app/components/Parking.jsx
+++ src/app/components/Parking.jsx
@@ -2,7 +2,7 @@
 
 import React from 'react'
 import dynamic from 'next/dynamic'
-import Navbar from '../Navbar'
+import Navbar from '../components/Navbar'
 import '../Parking.css'
 
 const Map = dynamic(
